ff79b8388a4dcc9738a636665a17f0cda3f5e4ce
someyaaltous <someyaaltous1995@gmail.com>
2020-02-03 18:54:33 +0200

    create component folder

---
+++ src/component/Chat/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Chat extends Component {
+ 
+  render() {
+    
+    return (
+      <>
+      <div>HI</div>
+      </>
+       
+    );
+  }
+}
+
+export default Chat;
