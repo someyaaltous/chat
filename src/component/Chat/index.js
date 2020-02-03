11c6df7569692e199c8eb52cb96c7b9aee11b685
someyaaltous <someyaaltous1995@gmail.com>
2020-02-03 19:19:40 +0200

    add chat component

--- src/component/Chat/index.js
+++ src/component/Chat/index.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 
 class Chat extends Component {
- 
-  render() {
-    
-    return (
-      <>
-      <div>HI</div>
-      </>
-       
-    );
-  }
+state = {
+          messages: ['msg1', 'msg2', 'msg3'],
+            allMessages: []
 }
+iterval;
 
+componentDidMount() {
+ let i = 0;
+        this.iterval = setInterval(() => {
+            this.setState({allMessages: [...this.state.allMessages, this.state.messages[i]]}, () => {
+                i++;
+            });
+        }, 2000);
+}
+    componentDidUpdate(prevProps: any) {
+        if(this.state.allMessages.length === 3) {
+            clearInterval(this.iterval)
+        }
+}
+    render() {
+        return (
+            <React.Fragment>
+                {this.state.allMessages.map((ele) => <li>{ele}</li>)}
+            </React.Fragment>
+        );
+    }
+  }
 export default Chat;
