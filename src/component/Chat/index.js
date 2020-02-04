9d6f4feda167e95d2b269b5196cb12aef3ff066a
someyaaltous <someyaaltous1995@gmail.com>
2020-02-04 12:45:46 +0200

    edite the style

--- src/component/Chat/index.js
+++ src/component/Chat/index.js
@@ -1,31 +1,74 @@
 import React, { Component } from 'react';
+import {
+  Listmass,
+  Yesbutton,
+  Threepoint,
+  Div,
+  Nobutton,
+  Container,
+  User,
+  UserDiv
+} from './index.style';
+import user from '../../assest/user.png';
 
 class Chat extends Component {
-state = {
-          messages: ['msg1', 'msg2', 'msg3'],
-            allMessages: []
-}
-iterval;
+  state = {
+    messages: ['This is still Aftab, right?'],
+    allMessages: [],
+    loading: false
+  };
+  iterval;
 
-componentDidMount() {
- let i = 0;
-        this.iterval = setInterval(() => {
-            this.setState({allMessages: [...this.state.allMessages, this.state.messages[i]]}, () => {
-                i++;
-            });
-        }, 2000);
-}
-    componentDidUpdate(prevProps: any) {
-        if(this.state.allMessages.length === 3) {
-            clearInterval(this.iterval)
+  componentDidMount() {
+    let i = 0;
+    setTimeout(() => {
+      this.setState({ loading: true });
+    }, 1000);
+    this.iterval = setInterval(() => {
+      this.setState(
+        {
+          allMessages: [...this.state.allMessages, this.state.messages[i]],
+          loading: false
+        },
+        () => {
+          i++;
         }
-}
-    render() {
-        return (
-            <React.Fragment>
-                {this.state.allMessages.map((ele) => <li>{ele}</li>)}
-            </React.Fragment>
-        );
+      );
+    }, 2500);
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      this.state.allMessages.length === 1 &&
+      prevState.allMessages.length !== this.state.allMessages.length
+    ) {
+      clearInterval(this.iterval);
+      this.setState({ loading: false });
+    } else if (prevState.allMessages.length !== this.state.allMessages.length) {
+      setTimeout(() => {
+        this.setState({ loading: true });
+      }, 1000);
     }
   }
+  render() {
+    return (
+      <>
+        <Container>
+          {this.state.loading && <Threepoint><div className="hh1"></div><div className="hh2"></div><div className="hh3"></div></Threepoint>}
+        </Container>
+        {this.state.allMessages.map(ele => (
+          <>
+            <UserDiv>
+              <Listmass>{ele}</Listmass>
+            </UserDiv>
+            <Div>
+              <Yesbutton>yeah I'm</Yesbutton>
+
+              <Nobutton>Nope it's someone else</Nobutton>
+            </Div>
+          </>
+        ))}
+      </>
+    );
+  }
+}
 export default Chat;
